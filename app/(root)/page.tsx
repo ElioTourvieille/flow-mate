d3bc96274051c0d08a120af13531462f332bad07
eliotourvieille <e.tourvieille@gmail.com>
2025-10-15 17:12:54 +0200

    feat: implement landing page header and hero

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,20 +3,27 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="min-h-screen from-slate-950 to-slate-900 bg-gradient-to-b dark">
+    <main className="min-h-screen from-slate-950 to-slate-900 bg-gradient-to-b dark py-16">
       <BackgroundBeams className="pointer-events-none" />
 
-      <section className="container mx-auto px-4 py-16">
+      <section className="container mx-auto px-10 py-20">
         <div className="flex flex-col gap-6">
           <h1 className="text-6xl font-extrabold max-w-lg text-transparent bg-clip-text bg-gradient-to-b from-white to-violet-300">
             Streamline your Freelance Workflow.
           </h1>
-          <p className="text-lg font-light text-violet-50 max-w-xl">
+          <p className="text-xl text-violet-50 max-w-xl my-4">
             Manage your client projects efficienctly with visual workflows,
             automated notifications, and seamless client communication or file
             sharing.
           </p>
-          <Button variant="primary" className="w-fit">Get Started</Button>
+          <div className="flex gap-8">
+            <Button variant="primary" size="lg" className="w-fit">
+              Get Started
+            </Button>
+            <Button variant="outline" size="lg" className="w-fit">
+              Learn More
+            </Button>
+          </div>
         </div>
       </section>
     </main>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,16 +5,16 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap cursor-pointer rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        primary: "bg-gradient-to-b from-violet-500 to-violet-600 cursor-pointer border-2 border-violet-400 text-primary-foreground hover:bg-gradient-to-b hover:from-violet-600 hover:to-violet-700 hover:scale-105",
+        primary: "bg-gradient-to-b from-violet-500 to-violet-600 border-2 border-violet-400 text-primary-foreground hover:bg-gradient-to-b hover:from-violet-600 hover:to-violet-700 hover:scale-105",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border-3 border-violet-800 text-violet-400 shadow-md hover:bg-violet-500 hover:text-foreground px-10 hover:scale-105",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
